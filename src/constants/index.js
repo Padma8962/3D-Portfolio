ba6eba76a8d11a5a7ef3e54e5c3a4d0b6a9d3534
Padma8962 <padmaja.sharma0073@gmail.com>
2026-08-20 21:46:34 +0530

    Update LinkedIn URL in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -257,7 +257,7 @@ const socialImgs = [
   },
   {
     name: "linkedin",
-    url: "https://www.linkedin.com/",
+    url: "www.linkedin.com/in/padmaja-sharma-83385b315",
     imgPath: "/images/linkedin.png",
   },
 ];
